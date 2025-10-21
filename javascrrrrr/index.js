fc76889e9f123676a479d8250180ba3385df74bd
faisxg <faisxdwyl@gmail.com>
2025-10-21 14:55:07 +0200

    javascripting intensifies

--- javascrrrrr/index.js
+++ javascrrrrr/index.js
@@ -1,3 +1,12 @@
 let sitat = "I aint praying for that new vet I'd rather have ma daaawgs back";
 document.getElementById("kaiCENAT").innerText = sitat;
-console.log(sitat.length)
+console.log(sitat.toUpperCase());
+
+let sted1 = (prompt("Skriv et stedsnavn:")).length;
+let sted2 = (prompt("Skriv et til stedsnavn:")).length;
+
+let toiletrizz = Math.abs(sted1-sted2);
+
+console.log(toiletrizz);
+
+document.getElementById("diff").innerText = "Differansen mellom bokstavene til stedsnavnene du har oppgitt er " + toiletrizz;
